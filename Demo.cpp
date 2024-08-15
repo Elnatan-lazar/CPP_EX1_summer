f12f3226c76cc69d834c45711306a5218b5cbb89
‫Elnatan Lazar <elnatan100@gmail.com>
2024-08-15 15:26:46 +0300

    startin from zero

--- Demo.cpp
+++ Demo.cpp
@@ -44,7 +44,6 @@ int main()
     cout << Algorithms::isContainsCycle(g) << endl;    // Should print: "The cycle is: 0->1->2->0".
     cout << Algorithms::isBipartite(g) << endl;        // Should print: "0" (false).
 
-
     // 5x4 matrix that reprsents invalid graph.
     vector<vector<int>> graph4 = {
         {0, 1, 2, 0},
@@ -61,15 +60,15 @@ int main()
         cout << e.what() << endl; // Should print: "Invalid graph: The graph is not a square matrix."
     }
 
-     ariel::Graph g1;
+    ariel::Graph g1;
     // 3x3 matrix that represents a connected graph.
-    vector<vector<int>> graph = {
+    vector<vector<int>> graph5 = {
         {0, 1, 0},
         {1, 0, 1},
         {0, 1, 0}};
-    g1.loadGraph(graph); // Load the graph to the object.
+    g1.loadGraph(graph5); // Load the graph to the object.
 
-    cout<<g1; // Should print the matrix of the graph: [0, 1, 0], [1, 0, 1], [0, 1, 0]
+    cout << g1; // Should print the matrix of the graph: [0, 1, 0], [1, 0, 1], [0, 1, 0]
 
     // 3x3 matrix that represents a weighted connected graph.
     vector<vector<int>> weightedGraph = {
@@ -80,24 +79,24 @@ int main()
     g2.loadGraph(weightedGraph); // Load the graph to the object.
 
     ariel::Graph g3 = g1 + g2; // Add the two graphs together.
-    cout<<g3;           // Should print the matrix of the graph: [0, 2, 1], [2, 0, 3], [1, 3, 0]
+    cout << g3;                // Should print the matrix of the graph: [0, 2, 1], [2, 0, 3], [1, 3, 0]
 
-    g1 *= -2;        // Multiply the graph by -2.
-    cout<<g1; // Should print the matrix of the graph: [0, -2, 0], [-2, 0, -2], [0, -2, 0]
+    g1 *= -2;   // Multiply the graph by -2.
+    cout << g1; // Should print the matrix of the graph: [0, -2, 0], [-2, 0, -2], [0, -2, 0]
 
     g1 /= -2;
     ariel::Graph g4 = g1 * g2; // Multiply the two graphs together.
-    cout<<g4;           // Should print the multiplication of the matrices of g1 and g2: [0, 0, 2], [1, 0, 1], [1, 0, 0]
+    cout << g4;                // Should print the multiplication of the matrices of g1 and g2: [0, 0, 2], [1, 0, 1], [1, 0, 0]
 
     // 5x5 matrix that represents a connected graph.
-    vector<vector<int>> graph2 = {
+    vector<vector<int>> graph6 = {
         {0, 1, 0, 0, 1},
         {1, 0, 1, 0, 0},
         {0, 1, 0, 1, 0},
         {0, 0, 1, 0, 1},
         {1, 0, 0, 1, 0}};
     ariel::Graph g5;
-    g5.loadGraph(graph2); // Load the graph to the object.
+    g5.loadGraph(graph6); // Load the graph to the object.
     try
     {
         ariel::Graph g6 = g5 * g1; // Multiply the two graphs together.
